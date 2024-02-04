37c023b0eb931082e5d877d1b312898e8a19e1e6
Scarborrower <94328979+Scarborrower@users.noreply.github.com>
2024-02-04 18:31:49 +0700

    fully setted up lsp

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -1,6 +1,44 @@
 return {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v3.x',
-    lazy = true,
-    config = false,
+    dependencies = {
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
+        'neovim/nvim-lspconfig',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/nvim-cmp',
+        'L3MON4D3/LuaSnip',
+    },
+
+    config = function()
+        local lsp_zero = require('lsp-zero')
+
+        lsp_zero.on_attach(function(client, bufnr)
+            -- see :help lsp-zero-keybindings
+            -- to learn the available actions
+            lsp_zero.default_keymaps({buffer = bufnr})
+        end)
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            ensure_installed = {'rust_analyzer'},
+            handlers = {
+                lsp_zero.default_setup,
+            },
+        })
+        local cmp = require('cmp')
+        local cmp_format = require('lsp-zero').cmp_format()
+        local cmp_action = require('lsp-zero').cmp_action()
+
+
+        cmp.setup({
+            mapping = cmp.mapping.preset.insert({
+                ['<Tab>'] = cmp_action.tab_complete(),
+                ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-d>'] = cmp.mapping.scroll_docs(4),   
+            })
+        })
+
+
+    end,
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,23 @@ local M = {
     build = function()
         require("nvim-treesitter.install").update({ with_sync = true })()
     end,
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            -- A list of parser names, or "all" (the five listed parsers should always be installed)
+            ensure_installed = { "c", "lua", "rust"},
+
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = true,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            highlight = {
+                enable = true,
+            },
+        }
+    end,
 }
 
 return { M }

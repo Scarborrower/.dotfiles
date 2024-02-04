fbff61232e64bc885cafa3d6ab0e21a97c93b503
Scarborrower <ng.k.dien123@gmail.com>
2024-02-04 13:15:57 +0700

    fixed an error bc for missing ','

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
 -- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+      dependencies = { 'nvim-lua/plenary.nvim' },
       config = function()
           local builtin = require('telescope.builtin')
           vim.keymap.set('n', '<leader>ff', builtin.find_files, {})

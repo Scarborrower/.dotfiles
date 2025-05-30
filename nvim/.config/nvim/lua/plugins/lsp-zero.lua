c3bd2fc4fe4d2f5c3fa8474ff52aa32a7735881a
Scarborrower <scarborrower@gmail.com>
2025-05-30 10:25:07 +0700

    enabled super tab in nvim

--- nvim/.config/nvim/lua/plugins/lsp-zero.lua
+++ nvim/.config/nvim/lua/plugins/lsp-zero.lua
@@ -26,15 +26,21 @@ return {
             },
         })
         local cmp = require('cmp')
---        local cmp_format = require('lsp-zero').cmp_format()
---        local cmp_action = require('lsp-zero').cmp_action()
+        --        local cmp_format = require('lsp-zero').cmp_format()
+        --        local cmp_action = require('lsp-zero').cmp_action()
         local cmp_autopairs = require('nvim-autopairs.completion.cmp')
         local cmp_select = {behavior = cmp.SelectBehavior.Select}
         cmp.setup({
 
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+
             cmp.event:on(
-            'confirm_done',
-            cmp_autopairs.on_confirm_done()
+                'confirm_done',
+                cmp_autopairs.on_confirm_done()
             ),
             preselect = 'item',
             completion = {
@@ -42,8 +48,33 @@ return {
             },
             mapping = cmp.mapping.preset.insert({
                 ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    local luasnip = require('luasnip')
+                    local col = vim.fn.col('.') - 1
+
+                    if cmp.visible() then
+                        cmp.select_next_item({behavior = 'select'})
+                    elseif luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                        fallback()
+                    else
+                        cmp.complete()
+                    end
+                end, {'i', 's'}),
+
+                -- Super shift tab
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
+                    local luasnip = require('luasnip')
+
+                    if cmp.visible() then
+                        cmp.select_prev_item({behavior = 'select'})
+                    elseif luasnip.locally_jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, {'i', 's'}),
                 ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-d>'] = cmp.mapping.scroll_docs(4),
             })
